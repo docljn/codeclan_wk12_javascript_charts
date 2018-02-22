611fdbf5d01c393c6590a90ccbe9c336c46c2541
DocLJN <ljnoble@gmail.com>
2018-02-22 11:08:28 +0000

    Pie chart displays correctly

---
+++ public/pieChart.js
@@ -0,0 +1,47 @@
+const PieChart = function () {
+  const container = document.querySelector("#pie-chart");
+
+  const chart = new Highcharts.Chart({
+    chart: {
+      type: "pie",
+      renderTo: container
+    },
+    title: {
+      text: "Pokemon Types I've caught"
+    },
+    series: [
+      {
+        name: "PokemonTypes",
+        data: [
+          {
+            name: "Fire",
+            y: 74,
+            color: "#ffac33"
+          },
+          {
+            name: "Water",
+            y: 25,
+            color: "#73b7ff",
+            sliced: true
+          },
+          {
+            name: "Grass",
+            y: 1,
+            color: "#00ba2f"
+          },
+          {
+            name: "Rock",
+            y: 15,
+            color: "#111111"
+          }
+        ]
+      }
+    ]
+  });
+
+
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  new PieChart();
+});
